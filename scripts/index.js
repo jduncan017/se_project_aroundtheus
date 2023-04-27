c55c37b64748b106452f0780a5aa478c6a232026
jduncan017 <emailjoshduncan@gmail.com>
2023-04-26 20:30:53 -0600

    added functionality for modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+// Add functionality for the edit profile edit button
+const editProfile = document.querySelector(".profile__name-button");
+const closeModal = document.querySelector(".modal__close-button");
+const modal = document.querySelector(".modal");
+
+editProfile.addEventListener("click", function () {
+  modal.classList.add("modal_opened");
+});
+
+closeModal.addEventListener("click", function () {
+  modal.classList.remove("modal_opened");
+});
