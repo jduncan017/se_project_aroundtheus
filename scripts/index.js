a6fe58557790d745b63c1c9c289ef8b40bfb951d
jduncan017 <emailjoshduncan@gmail.com>
2023-05-18 00:04:27 -0600

    add js functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,94 +1,165 @@
-// card templates:
-// delclaring array:
+// Initial image array:
 const initialCards = [
   {
-    name: "Yosemite Valley",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
   {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
   },
   {
-    name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
   },
 ];
 
-// selecting the card template & list(parent):
-const cardTemplate = document.querySelector("#cards").content;
+// Card Variables:
+const cardTemplate = document.querySelector("#cards").content.firstElementChild;
 const cardsList = document.querySelector(".cards__list");
+const imageModal = document.querySelector("#image-modal");
+const modalContainer = imageModal.querySelector(".image-modal__container");
+const modalTitle = imageModal.querySelector(".image-modal__title");
 
-// function declaration for getting card data:
-function getCardElement(data) {
-  // clone the template
-  let newCard = cardTemplate.cloneNode(true);
-  // select the elements
-  let cardImage = newCard.querySelector(".card__image");
-  let cardTitle = newCard.querySelector(".card__title");
-  // add content
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
-  cardTitle.textContent = data.name;
+// Function for adding images to page
+const addImages = (imageData) => {
+  imageData.forEach((card) => {
+    // clone the template
+    const newCard = cardTemplate.cloneNode(true);
+    // select the elements
+    const cardImage = newCard.querySelector(".card__image");
+    const cardTitle = newCard.querySelector(".card__title");
+    const cardLikeBtn = newCard.querySelector(".card__like-button");
+    const cardTrashBtn = newCard.querySelector(".card__trash-button");
 
-  return newCard;
-}
+    // add content to the elements
+    cardImage.src = card.link;
+    cardImage.alt = card.name;
+    cardTitle.textContent = card.name;
+    // Add event listeners to toggle like button:
+    cardLikeBtn.addEventListener("click", () => {
+      cardLikeBtn.classList.toggle("card__like-button_active");
+    });
+    // Add event listeners to delete card with trash button:
+    cardTrashBtn.addEventListener("click", () => {
+      const parentCard = cardTrashBtn.parentElement;
+      parentCard.remove();
+    });
+    // Add event listeners for image modals:
+    cardImage.addEventListener("click", () => {
+      const modalImage = imageModal.querySelector(".image-modal__image");
+      modalImage.src = card.link;
+      modalImage.alt = card.name;
+      imageModal.querySelector(".image-modal__title").textContent = card.name;
+      imageModal.classList.add("modal_opened");
+    });
 
-// iterating through cards data array:
-for (let i = 0; i < initialCards.length; i++) {
-  let cardElement = getCardElement(initialCards[i]);
-  cardsList.append(cardElement);
-}
+    // append to HTML
+    cardsList.prepend(newCard);
+  });
+};
 
-// Add functionality for the edit profile edit button
-const editProfile = document.querySelector(".profile__name-button");
-const closeModal = document.querySelector(".modal__close-button");
-const modal = document.querySelector(".modal");
+// add initial images to page:
+addImages(initialCards);
 
-editProfile.addEventListener("click", function () {
-  let profileName = document.querySelector(".profile__name-title").textContent;
-  let profileDescription = document.querySelector(
+// MODAL FUNCATIONALITY
+// Modal variable definitions:
+// (specific defniitions listed under each modal)
+const editModal = document.querySelector("#edit-modal");
+const addModal = document.querySelector("#add-image-modal");
+
+// GENERAL MODAL FUNCTIONS:
+// Open Functionality
+const openModal = (modal) => {
+  modal.classList.add("modal_opened");
+};
+
+// Close button functionality:
+const closeButtons = document.querySelectorAll(".modal__close-button");
+
+const closeModal = (modal) => {
+  modal.classList.remove("modal_opened");
+};
+
+closeButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const modal = button.closest(".modal");
+    closeModal(modal);
+  });
+});
+
+// PROFILE EDIT MODAL:
+// Variables:
+const editProfileBtn = document.querySelector(".profile__name-button");
+const profileFormElement = document.querySelector("#edit-profile-form");
+const profileEditCloseBtn = editModal.querySelector(".modal__close-button");
+
+// Functionality for the edit profile button
+editProfileBtn.addEventListener("click", function () {
+  const profileName = document.querySelector(
+    ".profile__name-title"
+  ).textContent;
+  const profileDescription = document.querySelector(
     ".profile__description"
   ).textContent;
-  modal.classList.add("modal_opened");
-  modal.querySelector("#name").value = profileName;
-  modal.querySelector("#description").value = profileDescription;
+  openModal(editModal);
+  editModal.querySelector("#name").value = profileName;
+  editModal.querySelector("#description").value = profileDescription;
 });
 
-closeModal.addEventListener("click", function () {
-  modal.classList.remove("modal_opened");
-});
-
-// find the form in the DOM
-const profileFormElement = document.querySelector(".edit-profile-form");
-
-// find the form fields in the DOM
-const nameInput = profileFormElement.querySelector("#name");
-const jobInput = profileFormElement.querySelector("#description");
-
-// find the profile elements in the DOM
-const profileName = document.querySelector(".profile__name-title");
-const profileJob = document.querySelector(".profile__description");
-
-// the form submission handler.
-function handleProfileFormSubmit(evt) {
+// Form submission handler.
+const handleProfileFormSubmit = (evt) => {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  modal.classList.remove("modal_opened");
-}
+  const profileNameInput = profileFormElement.querySelector("#name");
+  const profileJobInput = profileFormElement.querySelector("#description");
+  const profileName = document.querySelector(".profile__name-title");
+  const profileJob = document.querySelector(".profile__description");
+  profileName.textContent = profileNameInput.value;
+  profileJob.textContent = profileJobInput.value;
+  closeModal(editModal);
+};
 
-// connect the handler to the form:
+// Sumbit Button Listener
 profileFormElement.addEventListener("submit", handleProfileFormSubmit);
+
+// ADD IMAGE MODAL:
+// Variables:
+const addImageBtn = document.querySelector(".profile__add-button");
+const imageFormElement = document.querySelector("#add-image-form");
+
+// Functionality for the image add button
+addImageBtn.addEventListener("click", function () {
+  openModal(addModal);
+});
+
+// function for submitting images
+const handleImageFormSubmit = (evt) => {
+  evt.preventDefault();
+  const imageTitleInput = evt.target.title;
+  const imageLinkInput = evt.target.link;
+  const imageData = [
+    {
+      name: imageTitleInput.value,
+      link: imageLinkInput.value,
+    },
+  ];
+  addImages(imageData);
+  imageTitleInput.value = "";
+  imageLinkInput.value = "";
+  closeModal(addModal);
+};
+
+// event listener to submit image
+imageFormElement.addEventListener("submit", handleImageFormSubmit);
