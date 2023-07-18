83b7d3b232658ac052830c2b3ccee8b536771997
jduncan017 <emailjoshduncan@gmail.com>
2023-07-18 09:16:06 -0600

    Updates

--- src/utils/constants.js
+++
@@ -1,8 +0,0 @@
-export const settings = {
-  formSelector: ".popup__form",
-  inputSelector: ".popup__form-input",
-  submitButtonSelector: ".popup__submit-button",
-  inactiveButtonClass: "popup__submit-button_disabled",
-  inputErrorClass: "popup__input-invalid",
-  errorClass: "popup__error_visible",
-};
